e3df919fa218028ec54e25c43dbc3b7c0e05bc19
derek <derek.dianwei@gmail.com>
2018-11-09 22:16:10 -0500

    apply angular material to create post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { CreatePostComponent } from './posts/create-post/create-post.component';
-import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,9 @@ import { NoopAnimationsModule, BrowserAnimationsModule } from '@angular/platform
   imports: [
     BrowserModule,
     FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
     BrowserAnimationsModule,
     NoopAnimationsModule
   ],
